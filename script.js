4eed89ba77b2d5e108f67976a338d1180146cf55
chinky23-tech <chinky0330@gmail.com>
2025-06-30 23:17:43 +0530

    animation

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 gsap.to(".box-green", {rotation: 360, x:100, duration: 1});
-gsap.from(".box-purple",{rotation: -360, x:-100, duration:1});
-gsap.fromTo(".box-blue", {x: -100},{rotation:360, x: 100, duration:1});
+gsap.from(".box-purple",{rotation: -360, x:-100, duration:2});
+gsap.fromTo(".box-blue", {x: -360},{rotation:360, x: 100, duration:3});
+
+
+gsap.to(".smile",{rotation: 360, x: 100, duration:1});
+gsap.from(".sad",{rotation: -360, x: -100, duration:1});
+gsap.fromTo(".angry",{x:-100},{rotation: 360, x: 100, duration:1});
